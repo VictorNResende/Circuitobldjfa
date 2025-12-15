11d075f219cee7feffc6bdc778bcac2a3ef5f6cd
MarceloKelmer <marcelo.lima@claro.com.br>
2025-12-15 19:47:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import os
 # ================== CONFIGURAÇÕES ==================
 TIMEOUT_TCP = 5
 MAX_THREADS = 10
-PORTA_TESTE = 44137
+PORTA_TESTE = 49159
 ARQUIVO_PADRAO = "bld_jfa.xlsx"
 
 # ================== USUÁRIOS ==================
