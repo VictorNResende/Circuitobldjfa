4d934b29be26616705c5ee55f17b62b211c5c971
VictorNResende <106785111+VictorNResende@users.noreply.github.com>
2025-12-15 17:12:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import os
 # ================== CONFIGURAÇÕES ==================
 TIMEOUT_TCP = 2
 MAX_THREADS = 20
-PORTA_TESTE = 443
+PORTA_TESTE = 80
 ARQUIVO_PADRAO = "bld_jfa.xlsx"
 
 # ================== USUÁRIOS ==================
